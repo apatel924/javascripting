6d41b6e005438d6cfccc08b9d856203a7a959718
Ashish Patel <93980631+apatel924@users.noreply.github.com>
2023-01-26 04:27:26 -0700

    Create ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("List of ingredients");
+for (let i= 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+
+console.log("List of ingredients");
+let i = 0;
+while (i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+console.log("List of Ingredients backwards");
+for (let i = ingredients.length -1; i>=0; i--){
+  console.log(ingredients[i]);
+}
+
+
+
